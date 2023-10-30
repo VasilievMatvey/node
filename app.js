e21e9fd7bebb23a54d50bb49475e080165c305da
programmer2 <slishuZOVebatbAZOV@gmail.com>
2023-10-30 10:57:03 +0300

    fdsf

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-import express from "express";
+const express = require("express");
+const favicon = require("express-favicon");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = "3000";
 
-app.get("/as", function (req, res) {
-  res.end("Hello!!!");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(favicon(__dirname + "/public/favicon.ico"));
+app.get("/test", (req, res) => {
+  console.log("Прошли по пути /test");
+  res.end("/test");
+});
+console.log(__dirname + "/public/favicon.ico");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
