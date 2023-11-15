0d5ab93f67284c75eebabd7314664c287b0121dd
programmer2 <slishuZOVebatbAZOV@gmail.com>
2023-11-15 15:29:53 +0300

    15.11

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "views")));
+app.use(
+  "css/bootstrap-5.3.2",
+  express.static(
+    path.join(__dirname, "css/bootstrap-5.3.2/dist/css/bootstrap.min.css")
+  )
+);
 app.use(favicon(__dirname + "/public/favicon.ico"));
 
 app.get(routeTest, (req, res) => {
