07241661a53fbf4e7db36fb26d06b7cba84bbf16
programmer2 <slishuZOVebatbAZOV@gmail.com>
2024-04-02 11:50:48 +0300

    +ref

--- controllers/login.js
+++ controllers/login.js
@@ -10,50 +10,64 @@ exports.form = (req, res) => {
   res.render("loginForm", { title: "Login", messanger: messanger });
 };
 
-async function authentificate(dataForm, cb) {
+// async function authentificate(dataForm, cb) {
+//   try {
+//     const user = await User.findOne({
+//       where: { email: dataForm.email },
+//     });
+//     if (!user) return cb();
+//     const result = await bcrypt.compare(dataForm.password, user.password);
+//     if (result) return cb(null, user);
+//     return cb();
+//   } catch (err) {
+//     return cb(err);
+//   }
+// }
+
+exports.submit = async (req, res, next) => {
   try {
     const user = await User.findOne({
-      where: { email: dataForm.email },
+      where: { email: req.body.loginForm.email },
     });
-    if (!user) return cb();
-    const result = await bcrypt.compare(dataForm.password, user.password);
-    if (result) return cb(null, user);
-    return cb();
-  } catch (err) {
-    return cb(err);
+    if (!user) {
+      logger.error("User not found");
+      return res.redirect("/back");
+    } else {
+      bcrypt.compare(
+        req.body.loginForm.password,
+        user.password,
+        (error, result) => {
+          if (!result) {
+            res.error("Имя или пароль неверный");
+            logger.error("Имя или пароль неверный");
+            res.redirect("back");
+          }
+          req.session.userEmail = result.email;
+          req.session.userName = result.name;
+          logger.info("Пользователь вошёл в аккаунт");
+          //jwt generation
+          const jwtTime = process.env.JWTTIME;
+          const token = jwt.sign(
+            {
+              name: result.email,
+            },
+            process.env.JWTTOKENSECRET,
+            {
+              expiresIn: jwtTime,
+            }
+          );
+          // создание cookie для пользователя
+          res.cookie("jwt", token, { httpOnly: true, maxAge: jwtTime });
+          logger.info(
+            `Создан новый токен для ${result.email}, Токен: ${token}`
+          );
+          return res.redirect("/");
+        }
+      );
+    }
+  } catch (error) {
+    logger.error("ОшибкаД " + error);
   }
-}
-
-exports.submit = (req, res, next) => {
-  authentificate(req.body.loginForm, (error, data) => {
-    if (error) {
-      logger.error(`Произошла ошибка: ${error}`);
-      return next(error);
-    }
-    if (!data) {
-      res.error("Имя или пароль неверный");
-      logger.error("Имя или пароль неверный");
-      res.redirect("back");
-    }
-    req.session.userEmail = data.email;
-    req.session.userName = data.name;
-    logger.info("Пользователь вошёл в аккаунт");
-    //jwt generation
-    const jwtTime = process.env.JWTTIME;
-    const token = jwt.sign(
-      {
-        name: data.email,
-      },
-      process.env.JWTTOKENSECRET,
-      {
-        expiresIn: jwtTime,
-      }
-    );
-    // создание cookie для пользователя
-    res.cookie("jwt", token, { httpOnly: true, maxAge: jwtTime });
-    logger.info(`Создан новый токен для ${data.email}, Токен: ${token}`);
-    return res.redirect("/");
-  });
 };
 
 exports.logout = (req, res, next) => {
